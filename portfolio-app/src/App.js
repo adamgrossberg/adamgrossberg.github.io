540ec7adf3fc5a782672ac3cb60d2878e98a7578
adamgrossberg <adamgrossberg@gatech.edu>
2023-07-18 16:38:18 +0200

    added home page

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './pages/home'
-import Projects from './pages/projects'
-import Resume from './pages/resume'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Resume from './pages/Resume'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
--- portfolio-app/src/pages/home.js
+++ portfolio-app/src/pages/home.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import EmailIcon from '@mui/icons-material/Email'
+import GithubIcon from '@mui/icons-material/GitHub'
+import '../styles/Home.css'
 
 function Home() {
     return (
-        <div>
-            Home
+        <div className='home'>
+            <div className='about'>
+                <h2>Hi, my name is Adam.</h2>
+                <div className='prompt'>
+                    <p>
+                        A computer science student with a passion for learning and creating.
+                    </p>
+                    <LinkedInIcon />
+                    <EmailIcon />
+                    <GithubIcon />
+                </div>
+            </div>
+            <div className='skills'>
+                <h1>Skills</h1>
+                <ol className='list'>
+                    <li className='item'>
+                        <h2>Category</h2>
+                        <span>Skills</span>
+                    </li>
+                    <li className='item'>
+                        <h2>Category</h2>
+                        <span>Skills</span>
+                    </li>
+                </ol>
+            </div>
         </div>
     )
 }

529d04e5b12c14d447270cf85e3b90584d0537ef
adamgrossberg <adamgrossberg@gatech.edu>
2023-07-22 02:39:49 +0200

    cleanup

--- portfolio-app/src/App.js
+++ portfolio-app/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import ProjectDisplay from './pages/ProjectDisplay'
-import Resume from './pages/Resume'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import ProjectDisplay from './pages/ProjectDisplay';
+import Resume from './pages/Resume';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (

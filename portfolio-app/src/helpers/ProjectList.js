ac9b06bb39728c6ab84bcd89f95191f22d91781f
adamgrossberg <adamgrossberg@gatech.edu>
2023-07-22 02:02:24 +0200

    deleted unnecessary assets

--- portfolio-app/src/helpers/ProjectList.js
+++ portfolio-app/src/helpers/ProjectList.js
@@ -1,18 +1,14 @@
-import proj1 from '../assets/proj1.jpg'
-import proj2 from '../assets/proj2.jpg'
 
 export const ProjectList = [
     {
         name: 'Team Slackbot.',
         description: 'Slack app to increase productivity, motivation, and accountability of sports teams.',
-        image: proj1,
         skills: "Python, MongoDB, Slack API",
         github: "https://github.com/adamgrossberg/ultimate-slackbot"
     },
     {
         name: 'Simple Airline Management System.',
         description: 'Database system and object-oriented program to simulate and track simulated network of airports, airplanes, pilots, and passengers.',
-        image: proj2,
         skills: "MySQL, Database System Design, Java, OOP, JavaFX, Iterative Software Development",
         github: "https://github.com/adamgrossberg/simple-airline-management-system"
     }
